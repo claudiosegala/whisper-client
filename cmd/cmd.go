0b30c73743a2b2539a63472570f69b851a113c19
Claudio Segala Rodrigues Silva Filho <claudiosegalafilho@gmail.com>
2019-10-14 11:42:37 -0300

    Adapting initializers

--- cmd/cmd.go
+++ cmd/cmd.go
@@ -48,8 +48,8 @@ func initConfig() {
 
 // Run defines what should happen when the user runs 'whisper-client'
 func Run(cmd *cobra.Command, args []string) error {
-	flags := new(config.Config).InitFromViper(viper.GetViper())
-	whisperClient := new(client.WhisperClient).InitFromConfig(flags)
+	config := new(config.Config).InitFromViper(viper.GetViper())
+	whisperClient := new(client.WhisperClient).InitFromConfig(config)
 	t, err := whisperClient.CheckCredentials()
 	if err == nil { // store token
 		tokenJSONString := whisperClient.GetTokenAsJSONStr(t)
